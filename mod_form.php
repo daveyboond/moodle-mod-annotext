46e2a408cf8be6d06b445ef202fc3ca1946a453c
Steve Bond <s.bond1@lse.ac.uk>
2014-04-02 17:18:21 +0100

    Initial commit

---
+++ mod_form.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+
+require_once ($CFG->dirroot.'/course/moodleform_mod.php');
+
+class mod_annotext_mod_form extends moodleform_mod {
+
+}
+
